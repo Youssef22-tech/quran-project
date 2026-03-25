cb27fb9c57dd9cb4a55d9f27d89805a809a4279d
Youssef-tech22 <yousef.tt77@gmail.com>
2026-03-25 18:47:04 +0200

    update session config and env setup

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: false,
+        secure: true,
         httpOnly: true,
         sameSite: 'strict',
         maxAge: 7 * 24 * 60 * 60 * 1000
